ba9af738086f98c9c6e0e92b34be28a14ac46927
Maksym Nov <novodmaxim@gmail.com>
2023-01-21 10:11:05 +0100

    add background image

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
+import os
 pygame.init()
+
+def file_path(file_name):
+    folder_path = os.path.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
 
 FPS = 40 
 WIN_WIDTH = 700
@@ -7,6 +13,9 @@ WIN_HEIGHT = 500
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
+
+background = pygame.image.load(file_path("FF_zelf-iris-foto-maken-diy_01.jpg"))
+background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
 
 play = True
 game = True
@@ -16,5 +25,9 @@ while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
+    if play == True:
+        window.blit(background, (0, 0))
+
     clock.tick(FPS)
     pygame.display.update
