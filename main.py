98ebd3ae7418563d342fc60089c298c899c44909
Maksym Nov <novodmaxim@gmail.com>
2023-01-28 09:17:49 +0100

    add class GameSprite

--- main.py
+++ main.py
@@ -14,8 +14,22 @@ WIN_HEIGHT = 500
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
 
-background = pygame.image.load(file_path("FF_zelf-iris-foto-maken-diy_01.jpg"))
+background = pygame.image.load(file_path("завантаження.jpg"))
 background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, image, x, y,width, height, speed):
+        super().__init__()
+        self.image = pygame.image.load(file_path(image))
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 play = True
 game = True
@@ -30,4 +44,4 @@ while game == True:
         window.blit(background, (0, 0))
 
     clock.tick(FPS)
-    pygame.display.update
+    pygame.display.update()
