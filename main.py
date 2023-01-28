bbaa6f0b4fe1de2828a4afe66882813a6b77b90c
Maksym Nov <novodmaxim@gmail.com>
2023-01-28 10:03:03 +0100

    add Class Player

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 import os
 pygame.init()
@@ -19,11 +18,11 @@ background = pygame.image.load(file_path("завантаження.jpg"))
 background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
 
 class GameSprite(pygame.sprite.Sprite):
-    def __init__(self, image, x, y,width, height, speed):
+    def __init__(self, image, x, y, width, height, speed):
         super().__init__()
         self.image = pygame.image.load(file_path(image))
         self.image = pygame.transform.scale(self.image, (width, height))
-        self.rect = self.image.get_rect
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.speed = speed
@@ -31,6 +30,22 @@ class GameSprite(pygame.sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def __init__(self, image, x, y, width, height, speed):
+        super().__init__(image, x, y, width, height, speed)
+    
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+        
+    def fire(self):
+        pass
+    
+player = Player("images.png", 300, 600, 70, 70, 5)
+
 
 play = True
 game = True
@@ -44,39 +59,10 @@ while game == True:
     if play == True:
         window.blit(background, (0, 0))
 
-    clock.tick(FPS)
-=======
-import pygame
-import os
-pygame.init()
-
-def file_path(file_name):
-    folder_path = os.path.abspath(__file__ + "/..")
-    path = os.path.join(folder_path, file_name)
-    return path
-
-FPS = 40 
-WIN_WIDTH = 700
-WIN_HEIGHT = 500
-
-window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-clock = pygame.time.Clock()
-
-background = pygame.image.load(file_path("FF_zelf-iris-foto-maken-diy_01.jpg"))
-background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
-
-play = True
-game = True
 
 
-while game == True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game = False
-
-    if play == True:
-        window.blit(background, (0, 0))
+        player.reset()
+        player.update()
 
     clock.tick(FPS)
->>>>>>> 755d3d4c0ceaf2abe52f9a9e2d6af3924d35dd3e
     pygame.display.update()
